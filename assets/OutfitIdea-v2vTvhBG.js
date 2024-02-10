2ed4f7683398bc270fe3955708a68e7f546ba3cc
FiReBrO42 <93040967+FiReBrO42@users.noreply.github.com>
2024-02-10 14:48:51 +0000

    Update gh-pages to output generated at 2dd8302

---
+++ assets/OutfitIdea-v2vTvhBG.js
@@ -0,0 +1,1 @@
+import{_ as e,a as t,o as c}from"./index-mWYYBhV5.js";const n={};function o(r,a){return c(),t("div",null,"仙境啵啵ROBOBO2")}const _=e(n,[["render",o]]);export{_ as default};
--- src/page/OutfitIdea.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div>仙境啵啵 測試2</div>
-</template>
-<script setup></script>
-<style lang="scss" scoped></style>
